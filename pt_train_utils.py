1d6055ba489d536b0179bbcff3a59e17055e081c
vpushkarov <vladislav.pushkarov@agoda.com>
2020-04-09 19:10:24 +0300

    iterator impl

---
+++ pt_train_utils.py
@@ -0,0 +1,114 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.python.keras import callbacks as cbks
+from sklearn.model_selection import train_test_split
+
+def infer_shape_from_numpy_array(ary):
+  if len(ary.shape) == 1:
+    return (None,)
+  return (None,) + ary.shape[1:]
+
+def create_training_target(shape, dtype=None):
+  dtype = dtype or tf.int32
+
+  if shape[0] is None:
+    shape = shape[1:]
+
+  return tf.keras.layers.Input(shape, dtype=dtype)
+
+class Logger:
+  pass
+
+def prepare_data_iterables(x,
+                           y,
+                           validation_split=0.0,
+                           validation_data=None,
+                           batch_size=32,
+                           epochs=1,
+                           shuffle=True,
+                           shuffle_buf_size=1024,
+                           random_state=0):
+  if validation_split == 0.0 and validation_data is None:
+    return [DataIterable(x, y, batch_size, epochs, shuffle, shuffle_buf_size)]
+
+  elif validation_split == 0.0 and validation_data is not None:
+    train_dataset = DataIterable(x,
+                                 y,
+                                 batch_size,
+                                 epochs,
+                                 shuffle,
+                                 shuffle_buf_size)
+    test_dataset = DataIterable(validation_data[0],
+                                validation_data[1],
+                                batch_size,
+                                epochs,
+                                shuffle,
+                                shuffle_buf_size)
+    return [train_dataset, test_dataset]
+
+  elif  0.0 < validation_split < 1:
+    x_train, x_test, y_train, y_test = train_test_split(x, y,
+        test_size=validation_split, random_state=random_state)
+    train_dataset = DataIterable(x_train,
+                                 y_train,
+                                 batch_size,
+                                 epochs,
+                                 shuffle,
+                                 shuffle_buf_size)
+    test_dataset = DataIterable(x_test,
+                                y_test,
+                                batch_size,
+                                epochs,
+                                shuffle,
+                                shuffle_buf_size)
+    return [train_dataset, test_dataset]
+  else:
+    raise ValueError('Cannot parition data.')
+
+class DataIterable:
+  # TODO: Extend support for eager iteration
+  # Implement Wrapper for other than numpy arrays data types.
+
+  def __init__(self, x, y, batch_size=32, epochs=1, shuffle=True, shuffle_buf_size=1024):
+    self.x = x
+    self.y = y
+    self.batch_size = min(batch_size, y.shape[0])
+    self.epochs = epochs
+    self.shuffle = shuffle
+    self.shuffle_buf_size = shuffle_buf_size
+    self.__len = x.shape[0]
+    d = tf.data.Dataset.from_tensor_slices(
+        {
+            'x': self.x,
+            'y': self.y
+          })
+    d = d.repeat(self.epochs)
+    if self.shuffle:
+      d = d.shuffle(self.shuffle_buf_size)
+    d = d.batch(self.batch_size)
+
+    self._iterator = d.make_initializable_iterator()
+    self._next = self._iterator.get_next()
+
+  def __iter__(self):
+    if not tf.executing_eagerly():
+      sess = tf.compat.v1.keras.backend.get_session()
+      sess.run(self._iterator.initializer)
+      return _GraphModeIterator(self._next)
+    else:
+      raise NotImplementedError()
+
+  def __len__(self):
+    return self.__len
+
+class _GraphModeIterator:
+  def __init__(self, next_elem):
+    self.next_elem = next_elem
+
+  def __next__(self):
+    try:
+      sess = tf.compat.v1.keras.backend.get_session()
+      evaled = sess.run(self.next_elem)
+    except tf.compat.v1.errors.OutOfRangeError:
+      raise StopIteration()
+    return evaled['x'], evaled['y']
---
+++ pt_train_utils_test.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pt_train_utils as utils
+
+
+def iterator_test():
+  data_size = 10
+  x_data = np.random.normal(0, 1, size=(data_size, 1))
+  y_data = np.arange(data_size)
+  batch_size = 2
+  d = utils.DataIterable(x_data,
+                         y_data,
+                         batch_size=batch_size,
+                         epochs=2,
+                         shuffle=False)
+  i = 0
+  for (x, y) in d:
+    if i >= data_size:
+        i = 0
+    assert np.all(y == y_data[i: i + batch_size])
+    i += batch_size
+
+def test_prepare_data_iterables():
+  data_size = 10
+  x_data = np.random.normal(0, 1, size=(data_size, 2))
+  y_data = np.random.randint(0, 2, size=(data_size,))
+
+  # test that 1 dataset is returned when there is no validation split
+  datasets = pt_train_utils.prepare_data_iterables(x_data, y_data)
+  assert len(datasets) == 1
+  
+  # test that 2 datasets are returns when 1 > validation_split > 0
+  validation_split = 0.2
+  datasets = pt_train_utils.prepare_data_iterables(x_data,
+                                                   y_data,
+                                                   validation_split=validation_split)
+  assert len(datasets) == 2
+
+  # test that the test_size == validation_split * data_size and that
+  #               train_size == data_size - test_size and that
+  #               x_test.shape[0] == y_test.shape[0]
+  # (in that order)
+  validation_split = 0.2
+  datasets = pt_train_utils.prepare_data_iterables(x_data,
+                                                   y_data,
+                                                   validation_split=validation_split,
+                                                   shuffle=False)
+  train_dataset, test_dataset = datasets
+  results_x = []
+  results_y = []
+  for (x, y) in test_dataset:
+      results_x.append(x)
+      results_y.append(y)
+  x_test = np.concatenate(results_x, axis=0)
+  y_test = np.concatenate(results_y, axis=0)
+  
+  results_x = []
+  results_y = []
+  for (x, y) in train_dataset:
+      results_x.append(x)
+      results_y.append(y)
+  x_train = np.concatenate(results_x, axis=0)
+  y_train = np.concatenate(results_y, axis=0)
+  
+  assert x_test.shape[0] == validation_split * data_size
+  assert x_train.shape[0] == data_size - validation_split * data_size
+  assert x_test.shape[0] == y_test.shape[0]
